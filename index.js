b5a3ae338bf5961f856bfbe15882017b1375a1e1
sai-teja-kst <karrisaiteja0234@gmail.com>
2025-03-09 17:39:34 +0900

    1.0.5

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const GenerateData = require("./modules/generator");
 const PublishData = require("./modules/publisher");
 
 try {
-    cron.schedule("* * * * *", () => {
+    cron.schedule("0 * * * * *", () => {
         let msg = GenerateData();
         if (msg) {
             PublishData(msg);
